377f6414603134eb6c63372b4b30427a9f68b8d6
KossySteve <kossyeze@gmail.com>
2022-06-07 16:28:57 -0700

    connection files editted

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,9 @@
-// // Update with your config settings.
+// Update with your config settings.
 
-// /**
-//  * @type { Object.<string, import("knex").Knex.Config> }
-//  */
-// module.exports = {
-//   development: {
-//     client: 'mysql',
-//     connection: {
-//       host: '127.0.0.1',
-//       user: 'root',
-//       password: ':rootroot',
-//       database: 'injectdb',
-//       charset: 'utf8'
-//     },
-//   },
-// };
-
-const connections = {
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
   development: {
     client: 'mysql',
     connection: {
@@ -27,13 +14,33 @@ const connections = {
       charset: 'utf8'
     },
   },
+};
+
+module.exports = {
   production: {
     client: 'mysql',
     connection: process.env.JAWSDB_URL,
-  },
+  }
 };
 
-module.exports = 
-  process.env.NODE_ENV === 'production'
-    ? connections.production
-    : connections.development;
+// const connections = {
+//   development: {
+//     client: 'mysql',
+//     connection: {
+//       host: '127.0.0.1',
+//       user: 'root',
+//       password: ':rootroot',
+//       database: 'injectdb',
+//       charset: 'utf8'
+//     },
+//   },
+//   production: {
+//     client: 'mysql',
+//     connection: process.env.JAWSDB_URL,
+//   },
+// };
+
+// module.exports = 
+//   process.env.NODE_ENV === 'production'
+//     ? connections.production
+//     : connections.development;
